2df120956ff2e545e64785a45ba60aa6190ba6f7
Riya-t-21 <102308368+Riya-t-21@users.noreply.github.com>
2023-06-04 23:59:44 +0530

    Building Sidebar Component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className='h-100 pt-2'>
+        <DashboardToggle />
+
+
+    </div>
+  );
+};
+
+export default Sidebar
